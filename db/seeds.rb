cf8385c2cde11b0d6fd56ba3fc5bd0589622ac9f
Aline Mokfa <alinemokfa@hotmail.co.uk>
2017-10-15 17:40:47 +0100

    created customers and films tables + SqlRunner + Customer class and seeds

---
+++ db/seeds.rb
@@ -0,0 +1,19 @@
+require 'pry'
+require_relative '../models/customer.rb'
+
+
+Customer.delete_all
+
+cust_1 = Customer.new(
+  {'name' => 'Aline',
+  'funds' => '500'
+  }
+ )
+cust_1.save()
+
+cust_2 = Customer.new(
+  {'name' => 'Patrick',
+  'funds' => '500'
+  }
+ )
+cust_2.save()
