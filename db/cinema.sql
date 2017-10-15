baf434f02fe356c748deafefd033a950db0a4f30
Aline Mokfa <alinemokfa@hotmail.co.uk>
2017-10-15 17:30:43 +0100

    created customers and films tables + SqlRunner

---
+++ db/cinema.sql
@@ -0,0 +1,15 @@
+DROP TABLE customers;
+DROP TABLE films;
+
+
+CREATE TABLE customers (
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  funds INT4
+);
+
+CREATE TABLE films (
+  id SERIAL4 PRIMARY KEY,
+  title VARCHAR(255),
+  price INT4
+);
