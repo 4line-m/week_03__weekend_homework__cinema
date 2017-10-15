bd79a1df25973b5d82bc471716eb464ca631ddad
Aline Mokfa <alinemokfa@hotmail.co.uk>
2017-10-15 21:11:26 +0100

    added films table + created Film class + added films to seeds file + passed save method

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 require 'pry'
 require_relative '../models/customer.rb'
+require_relative '../models/film.rb'
 
 Customer.delete_all
 
@@ -13,9 +14,22 @@ cust_1.save()
 cust_2 = Customer.new(
   {'name' => 'Patrick',
   'funds' => '500'
+  })
+cust_2.save()
+
+film_1 = Film.new(
+  {'title' => 'Chicken Run: Why Did Margaret Cross the Road?',
+  'price' => '10'
   }
  )
-cust_2.save()
+film_1.save()
+
+film_2 = Film.new(
+  {'title' => 'Finding Margaret',
+  'price' => '10'
+  }
+ )
+film_2.save()
 
 binding.pry
 nil
